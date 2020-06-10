15f45c9a24676520112ff2ab738e9fc8e5444e42
Oksanteel <habanera@mail.ru>
2020-06-10 11:02:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 10248;
+        int mile = 1;
+        int spent = 20;
+
+        int bonus = ticket / spent * mile;
+        System.out.println(bonus);    }
+    }
